f8867d1cac915bb7298ef7306a050f01d6ce1ad8
zobekenobe <zobekenobe@windows_laptop>
2022-09-13 15:00:44 +0100

    moving show_about into the conditional

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from model import show_predict
 
 st.set_page_config(page_title = 'Stackoverflow Salary (2022) Prediction', layout = 'centered')
 
-show_about()
+
 page = st.sidebar.selectbox(label = 'Page', options = ['About', 'Predict', 'about me'])
-if page == 'Predict':
+if page == 'About':
+    show_about()
+elif page == 'Predict':
     show_predict()
+    
